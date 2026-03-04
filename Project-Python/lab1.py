d448b8baadffb10d90eacce222d19b7862bdb530
Phantosaur <arseniyberdsk4@gmail.com>
2026-03-04 12:21:44 +0700

    Update lab1.py

--- Project-Python/lab1.py
+++ Project-Python/lab1.py
@@ -4,7 +4,7 @@ import time
 import random
 
 from ev3dev2.button import Button
-led=Leds()
+led=Leds() 
 led.all_off() # Turn all LEDs off
 time.sleep(1)
 but = Button()
@@ -12,6 +12,7 @@ mybutt=""
 mini_level=[]
 show=1
 myvib=0
+game = 1
 butt = ["up","down","left","right"]
 def up(time1):
     led.set_color("LEFT", "GREEN")
@@ -33,6 +34,24 @@ def right(time1):
     led.set_color("RIGHT", "RED")
     time.sleep(time1)
     led.all_off()
+def no(time1):
+    led.set_color("RIGHT", "RED")
+    led.set_color("LEFT", "RED")
+    time.sleep(time1)
+    led.all_off()
+    led.set_color("RIGHT", "RED")
+    led.set_color("LEFT","RED")
+    time.sleep(time1)
+    led.all_off()
+def yes(time1):
+    led.set_color("RIGHT", "GREEN")
+    led.set_color("LEFT", "GREEN")
+    time.sleep(time1)
+    led.all_off()
+    led.set_color("RIGHT", "GREEN")
+    led.set_color("LEFT","GREEN")
+    time.sleep(time1)
+    led.all_off()
 def showbut(butt,time1):
     if butt =="left":
         left(time1)
@@ -42,29 +61,64 @@ def showbut(butt,time1):
         up(time1)
     elif butt =="down":
         down(time1)
+    elif butt =="yes":
+        yes(time1)
+    elif butt =="no":
+        no(time1)    
 
 
-for i in range(5):
+for  i in range(5):
     mini_level.append(random.choice(butt))
 for i in range(len(mini_level)):
     if (show):
         for j in range(len(mini_level)):
             showbut(mini_level[j],2)
         show =0
-    while(1):
-        
-        if but.Up:
+    while(1):    
+        if but.up:
             showbut("up",2)
-            if 
+            if mini_level[i] == "up":
+                game=1
+                break
+            else:
+                game =-1
+                break    
+        if but.down:
+            showbut("down",2)
+            if mini_level[i] == "down":
+                game=1
+                break
+            else:
+                game =-1
+                break
+        if but.left:
+            showbut("left",2)
+            if mini_level[i] == "left":
+                game=1
+                break
+            else:
+                game =-1                      
+        if but.right:
+            showbut("right",2)
+            if mini_level[i] == "right":
+                game=1
+                break
+            else:
+                game =-1
+    if game == -1:
+        showbut(no)
+        exit()
+        
+         
         
 
         
 
 
-if mini_level == mybutt:
-    led.set_color("RIGHT", "GREEN")
-    led.set_color("LEFT", "RED")
-time.sleep(10)
+# if mini_level == mybutt:
+#     led.set_color("RIGHT", "GREEN")
+#     led.set_color("LEFT", "RED")
+# time.sleep(10)
 
         
 
